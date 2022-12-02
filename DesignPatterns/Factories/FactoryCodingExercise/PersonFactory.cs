8aece9a596ba8d9dda0673e25d30fa7b2bd37f20
ales.vesely <ales.vesely@dataprojekt.cz>
2022-12-02 19:59:31 +0100

    Exercise

---
+++ DesignPatterns/Factories/FactoryCodingExercise/PersonFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Factories.FactoryCodingExercise
+{
+    public class PersonFactory
+    {
+        private readonly IList<WeakReference<Person>> _persons = new List<WeakReference<Person>>();
+
+        public Person CreatePerson(string name)
+        {
+            var person = new Person
+            {
+                Name = name,
+                Id = _persons.Count
+            };
+
+            _persons.Add(new WeakReference<Person>(person));
+            return person;
+        }
+    }
+}
