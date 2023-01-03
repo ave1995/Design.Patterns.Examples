b04345ded5d6686bd170abf5ecafbb6956560de0
ales.vesely <ales.vesely@dataprojekt.cz>
2023-01-03 18:40:25 +0100

    ViewModel

---
+++ DesignPatterns/Proxy/ViewModel/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Proxy.ViewModel
+{
+    // Model
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+    }
+}
---
+++ DesignPatterns/Proxy/ViewModel/PersonViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Proxy.ViewModel
+{
+    public class PersonViewModel
+        : INotifyPropertyChanged
+    {
+        private readonly Person person;
+
+        public PersonViewModel(Person person)
+        {
+            this.person = person;
+        }
+
+        public string FirstName
+        {
+            get => person.FirstName;
+            set
+            {
+                if (person.FirstName == value) return;
+                person.FirstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get => person.LastName;
+            set
+            {
+                if (person.LastName == value) return;
+                person.LastName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // Decorator functionality (augments original object)
+        // Project two properties together into, e.g., an edit box.
+        public string FullName
+        {
+            get => $"{FirstName} {LastName}".Trim();
+            set
+            {
+                if (value == null)
+                {
+                    FirstName = LastName = null;
+                    return;
+                }
+                var items = value.Split();
+                if (items.Length > 0)
+                    FirstName = items[0]; // may cause npc
+                if (items.Length > 1)
+                    LastName = items[1];
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //[NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(
+          [CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this,
+              new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
