9f75974ea23ee02a694509f8a6aaef40a95fa9ef
ales.vesely <ales.vesely@dataprojekt.cz>
2023-01-08 13:41:55 +0100

    Undo Redo

--- DesignPatterns/Memento/MementoInitialization.cs
+++ DesignPatterns/Memento/MementoInitialization.cs
@@ -13,16 +13,16 @@ namespace DesignPatterns.Memento
         public static void BasicMementoInit()
         {
             var ba = new BankAccount(100);
-            var m1 = ba.Deposit(50); // 150
-            var m2 = ba.Deposit(25); // 175
+            ba.Deposit(50);
+            ba.Deposit(25);
             WriteLine(ba);
 
-            // restore to m1
-            ba.Restore(m1);
-            WriteLine(ba);
-
-            ba.Restore(m2);
-            WriteLine(ba);
+            ba.Undo();
+            WriteLine($"Undo 1: {ba}");
+            ba.Undo();
+            WriteLine($"Undo 2: {ba}");
+            ba.Redo();
+            WriteLine($"Redo 2: {ba}");
         }
     }
 }
--- DesignPatterns/Memento/SimpleMemento/BankAccount.cs
+++ DesignPatterns/Memento/SimpleMemento/BankAccount.cs
@@ -9,21 +9,54 @@ namespace DesignPatterns.Memento.SimpleMemento
     public class BankAccount
     {
         private int balance;
+        private List<Memento> changes = new List<Memento>();
+        private int current;
 
         public BankAccount(int balance)
         {
             this.balance = balance;
+            changes.Add(new Memento(balance));
         }
 
         public Memento Deposit(int amount)
         {
             balance += amount;
-            return new Memento(balance);
+            var m = new Memento(balance);
+            changes.Add(m);
+            ++current;
+            return m;
         }
 
         public void Restore(Memento m)
         {
-            balance = m.Balance;
+            if (m != null)
+            {
+                balance = m.Balance;
+                changes.Add(m);
+                current = changes.Count - 1;
+            }
+        }
+
+        public Memento Undo()
+        {
+            if (current > 0)
+            {
+                var m = changes[--current];
+                balance = m.Balance;
+                return m;
+            }
+            return null;
+        }
+
+        public Memento Redo()
+        {
+            if (current + 1 < changes.Count)
+            {
+                var m = changes[++current];
+                balance = m.Balance;
+                return m;
+            }
+            return null;
         }
 
         public override string ToString()
