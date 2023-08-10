be16ccbf0eb0a1e912b959548809212a9d00667f
ales.vesely <veselya95@gmail.com>
2023-08-10 23:09:39 +0200

    Discriminant

---
+++ DesignPatterns/Strategy(Policy)/Discriminant/DiscriminantStrategy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Strategy_Policy_.Discriminant
+{
+    public interface IDiscriminantStrategy
+    {
+        double CalculateDiscriminant(double a, double b, double c);
+    }
+
+    public class OrdinaryDiscriminantStrategy : IDiscriminantStrategy
+    {
+        public double CalculateDiscriminant(double a, double b, double c)
+        {
+            return b * b - 4 * a * c;
+        }
+    }
+
+    public class RealDiscriminantStrategy : IDiscriminantStrategy
+    {
+        //(return NaN on negative discriminant!)
+        public double CalculateDiscriminant(double a, double b, double c)
+        {
+            double result = b * b - 4 * a * c;
+            return result >= 0 ? result : double.NaN;
+        }
+    }
+
+    public class QuadraticEquationSolver
+    {
+        private readonly IDiscriminantStrategy strategy;
+
+        public QuadraticEquationSolver(IDiscriminantStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public Tuple<Complex, Complex> Solve(double a, double b, double c)
+        {
+            var disc = new Complex(strategy.CalculateDiscriminant(a, b, c), 0);
+            var rootDisc = Complex.Sqrt(disc);
+            return Tuple.Create(
+              (-b + rootDisc) / (2 * a),
+              (-b - rootDisc) / (2 * a)
+            );
+        }
+    }
+}
